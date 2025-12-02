c6828519ce26e5978c74e482b729c85631757786
niata <nia.talley2020@gmail.com>
2025-12-02 12:52:47 -0500

    Second commit

--- main.py
+++ main.py
@@ -1,2 +1,54 @@
+import data.py
+import helpers.py
+
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        # Check if the server is reachable before running tests
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print('Connect to Urban Routes server')
+        else:
+            print("Cannot connect to Urban Routes. Check the server is on and still running")
+
+    def test_set_route(self):
+        # Add in S8
+        print('function created for set route')
+        pass
+
+    def test_select_plan(self):
+        # Add in S8
+        print('function created for select plan')
+        pass
+
+    def test_fill_phone_number(self):
+        # Add in S8
+        print('function created for fill phone number')
+        pass
+
+    def test_fill_card(self):
+        # Add in S8
+        print('function created for fill card')
+        pass
+
+    def test_comment_for_driver(self):
+        # Add in S8
+        print('function created for comment for driver')
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Add in S8
+        print('function created for order blanket and handkerchiefs')
+        pass
+
+    def test_order_2_ice_creams(self):
+        # Add in S8
+        print('function created for order 2 ice creams')
+        for i in range(2):
+            # Add in S8
+            pass
+
+    def test_car_search_model_appears(self):
+        # Add in S8
+        print('function created for car search model')
+        pass
